1596d5204304fea9ede7111d6cc87020ddaf0c14
Lim2009 <keannenr@gmail.com>
2023-05-27 21:29:08 +0700

    dummy

---
+++ datadummy questions.sql
@@ -0,0 +1,9 @@
+--data dummy
+INSERT INTO Questions (dataType,question,[timeStamp],isAlive)
+VALUES('N','age','2023-05-27 14:01:19', 1)
+
+INSERT INTO Questions (dataType,question,[timeStamp],isAlive)
+VALUES('T','name','2023-05-27 14:01:19', 1)
+
+INSERT INTO Questions (dataType,question,[timeStamp],isAlive)
+VALUES('D','lahir','2023-05-27 14:01:19', 1)
