c097f55883f283e1d4e36baefdde5f698ca92ec0
vitofevrier <vitofev15202@gmail.com>
2023-06-12 16:37:10 +0700

    update sp delete form

--- spDeleteForm.sql
+++ spDeleteForm.sql
@@ -1,14 +1,27 @@
 ALTER PROCEDURE spDeleteForm
-	@idForm int
-
+	@idUser INT,
+	@idForm INT
 AS
+	DECLARE 
+		@prevTitle VARCHAR(128)
+
+	SELECT
+		@prevTitle=title
+	FROM
+		Form
+	WHERE
+		idForm=@idForm
+
 	BEGIN TRANSACTION
-		UPDATE
-			Form
+		UPDATE Form
 		SET
 			isAlive = 0
 		WHERE
 			idForm = @idForm
+
+		EXEC spChangeForm @idUser, @idForm, ''
 	COMMIT TRANSACTION
 
--- EXEC spDeleteForm 1
+--SELECT * FROM Form
+--SELECT * FROM LogChangeForm
+--EXEC spDeleteForm 1, 3
